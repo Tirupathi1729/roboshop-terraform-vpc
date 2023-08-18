d30d8dfd9f82d46a3d60d6908c2d0b92d923d965
Tirupathi1729 <kakarlatirupathireddy@gmail.com>
2023-08-18 10:03:32 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
-module "components" {
+module "vpc" {
   source = "git::https://github.com/Tirupathi1729/tf-module-basic-vpc.git"
 
   for_each = var.vpc
   cidr = each.value["cidr"]
   subnets = each.value["subnets"]
+}
+output "vpc" {
+  value = module.vpc
 }
